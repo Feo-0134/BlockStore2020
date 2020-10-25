127e49de31993d4852a737e117959263a44edbf9
JunCheng Zhu <junczhu@microsoft.com>
2020-10-25 10:05:41 +0800

    demo feature

--- examples/drizzle-react-tutorial/client/src/index.js
+++ examples/drizzle-react-tutorial/client/src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import drizzle functions and contract artifact
+import { Drizzle } from "@drizzle/store";
+import MyStringStore from "./contracts/MyStringStore.json";
+
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [MyStringStore],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545",
+    },
+  },
+};
+
+// setup drizzle
+const drizzle = new Drizzle(options);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App drizzle={drizzle} />
   </React.StrictMode>,
   document.getElementById('root')
 );
